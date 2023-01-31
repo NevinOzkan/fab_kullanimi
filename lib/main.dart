3bc410f812b3dd0961b0a744179fb3540f4da781
NevinOzkan <ozkannevin73@gmail.com>
2023-01-31 18:57:36 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Yazınız",
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: (() {
+                print("Fab 2 Tıklandı");
+              }),
+              tooltip: "Fab2",
+              child: const Icon(Icons.print),
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.white,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() {
+          print("Fab 1 Tıklandı");
+        }),
+        child: const Icon(Icons.audiotrack),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
